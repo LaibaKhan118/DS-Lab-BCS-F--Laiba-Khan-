cd94e8e3e832dcc8c9616abe699f5f835f1c37f6
Laiba Khan <80578072+LaibaKhan118@users.noreply.github.com>
2025-08-29 13:00:17 +0500

    Refactor nested loops for student input handling

--- Lab2/Q3.cpp
+++ Lab2/Q3.cpp
@@ -1,48 +1,34 @@
 #include <iostream>
 using namespace std;
-
-int main()
-{
+int main(){
     int students;
     cout << "Enter the number of students: ";
     cin >> students;
-
     int **marks = new int *[students];
     int *courses = new int[students];
-
-    for (int i = 0; i < students; i++)
-    {
+    for (int i = 0; i < students; i++){
         cout << "Enter the number of courses of student " << i << ": ";
         cin >> courses[i];
     }
-
-    for (int i = 0; i < students; i++)
-    {
+    for (int i = 0; i < students; i++){
         marks[i] = new int[courses[i]];
     }
-
-    for (int i = 0; i < students; i++)
-    {
+    for (int i = 0; i < students; i++){
         cout << "Student " << i << endl;
-        for (int j = 0; j < courses[i]; j++)
-        {
+        for (int j = 0; j < courses[i]; j++){
             cout << "Enter the marks of course " << j << ": ";
             cin >> marks[i][j];
         }
     }
-
     int sum = 0;
     cout << "Average Marks: " << endl;
-    for (int i = 0; i < students; i++)
-    {
+    for (int i = 0; i < students; i++){
         cout << "Student " << i << ": ";
-        for (int j = 0; j < courses[i]; j++)
-        {
+        for (int j = 0; j < courses[i]; j++){
             sum += marks[i][j];
         }
         int avg = sum / courses[i];
         cout << avg << endl;
     }
-
     return 0;
 }
