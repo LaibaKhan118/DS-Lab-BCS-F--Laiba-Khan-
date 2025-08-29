c978848dda1dd14818821516c6ee89e7f418f98a
Laiba Khan <80578072+LaibaKhan118@users.noreply.github.com>
2025-08-29 11:44:22 +0500

    Implement dynamic array input in Q1.cpp

---
+++ Lab2/Q1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int size;
+    cout << "Enter size: ";
+    cin >> size;
+
+    int *arr = new int[size]{};
+    int i;
+    char c;
+    while (true)
+    {
+        cout << "Enter the index: ";
+        cin >> i;
+        if (i < 0 || i > size)
+        {
+            cout << "The index must be between 0 and " << size << endl;
+            continue;
+        }
+        cout << "Enter the value: ";
+        cin >> arr[i];
+        cout << "do you want to enter another value (y/n)? ";
+        cin >> c;
+        if (c == 'N' || c == 'n')
+        {
+            break;
+        }
+    }
+
+    cout << "The final Array: " << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << endl;
+    }
+
+    delete[] arr;
+    return 0;
+}
