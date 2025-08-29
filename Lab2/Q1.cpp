e1f14d6bfe3c6fc74de0039674dce8ec2ba6bc39
Laiba Khan <80578072+LaibaKhan118@users.noreply.github.com>
2025-08-29 12:58:39 +0500

    Update Q1.cpp

--- Lab2/Q1.cpp
+++ Lab2/Q1.cpp
@@ -1,21 +1,16 @@
 #include <iostream>
 using namespace std;
-
-int main()
-{
+int main(){
     int size;
     cout << "Enter size: ";
     cin >> size;
-
     int *arr = new int[size]{};
     int i;
     char c;
-    while (true)
-    {
+    while (true){
         cout << "Enter the index: ";
         cin >> i;
-        if (i < 0 || i > size)
-        {
+        if (i < 0 || i > size) {
             cout << "The index must be between 0 and " << size << endl;
             continue;
         }
@@ -23,18 +18,14 @@ int main()
         cin >> arr[i];
         cout << "do you want to enter another value (y/n)? ";
         cin >> c;
-        if (c == 'N' || c == 'n')
-        {
+        if (c == 'N' || c == 'n'){
             break;
         }
     }
-
     cout << "The final Array: " << endl;
-    for (int i = 0; i < size; i++)
-    {
+    for (int i = 0; i < size; i++){
         cout << arr[i] << endl;
     }
-
     delete[] arr;
     return 0;
 }
