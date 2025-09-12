44dacaf2de6cde84b722ff2ae8c5f7583ca45d6f
Laiba Khan <80578072+LaibaKhan118@users.noreply.github.com>
2025-09-12 11:39:26 +0500

    Add bubble sort implementation in Q1.cpp
    Implement a simple bubble sort algorithm to sort an array in descending order.

---
+++ Lab4/inLab/Sorting/Q1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int size;
+    cout << "Enter the size of array: ";
+    cin >> size;
+    int *arr = new int[size]{};
+    for(int i=0; i < size; i++) {
+        cout << "Enter element: " << i+1 << ": ";
+        cin >> arr[i];
+    }
+    
+    for(int i = 0; i < size; i++) {
+        for(int j= 0; j < size-1; j++) {
+            if(arr[j] < arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    
+    cout << "Sorted Array:" << endl;
+    for(int i=0; i < size; i++) {
+        cout << arr[i] << endl;
+    }
+    
+    delete[] arr;
+    return 0;
+}
