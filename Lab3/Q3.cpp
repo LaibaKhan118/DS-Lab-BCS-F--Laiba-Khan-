3780ae395c7ac9ef4035619306711aeea41d56f3
Laiba Khan <80578072+LaibaKhan118@users.noreply.github.com>
2025-09-14 02:46:46 +0500

    Implement linked list with group reversal

---
+++ Lab3/Q3.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int val) : data(val), next(nullptr) {}
+};
+
+class LinkedList
+{
+private:
+    Node *head;
+
+    Node* reverse(Node *headNode, int k)
+    {
+        Node *previous= nullptr;
+        Node *current= headNode;
+        Node *next = nullptr;
+        int count =0;
+        Node* temp= headNode;
+        int nCount = 0;
+        while(temp && nCount < k) {
+            temp = temp->next;
+            nCount++;
+        }
+        if(nCount < k) return headNode;
+        while (current && count<k)
+        {
+            next = current->next;
+            current->next = previous;
+            previous = current;
+            current = next;
+            count++;
+        }
+        if(next) headNode->next = reverse(next, k);
+        return previous;
+    }
+
+public:
+    LinkedList() : head(nullptr) {}
+
+    void insertEnd(int val)
+    {
+        Node *newNode = new Node(val);
+        if (!head)
+        {
+            head = newNode;
+            return;
+        }
+        Node *temp = head;
+        while (temp->next)
+        {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+    void reverseInGroups(int k) { head = reverse(head, k); }
+    void print()
+    {
+        Node *temp = head;
+        while (temp)
+        {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+
+    
+};
+
+int main()
+{
+    LinkedList list;
+    for(int i=0; i<9;i++){
+        list.insertEnd(i);
+    }
+
+    list.reverseInGroups(3);
+
+    cout << "Merged List" << endl;
+    list.print();
+    return 0;
+}
