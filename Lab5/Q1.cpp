1a21b3f008d3898000fcd23c5e35108aba08f35a
Laiba Khan <80578072+LaibaKhan118@users.noreply.github.com>
2025-10-01 00:57:59 +0500

    Update Q1.cpp

--- Lab5/Q1.cpp
+++ Lab5/Q1.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
-using namespace std; 
-int calculateFactorial(int n){
-    if(n==0||n==1){
-      return 1;
+using namespace std;
+
+int calculateFactorial(int n) {
+    if(n==1||n==0){
+        return 1;
     }
-    else{ return n*calculateFactorial(n-1); }
+    return n*(calculateFactorial((n-1)));
 }
-int main() {
-  int n;
-  cout<< "Enter the number for factorial calculation: ";
-  cin>>n;
-  int result;
-  result = calculateFactorial(n);
-  cout<<"Factorial of "<<n<<" is "<<result<<endl;
-  return 0;
+
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    int result= calculateFactorial(n);
+    cout <<"Fatorial: " <<result<<endl;
+    return 0;
 }
