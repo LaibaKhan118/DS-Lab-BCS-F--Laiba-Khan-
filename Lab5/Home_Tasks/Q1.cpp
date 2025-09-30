bf16840baab985a5011ae676a66cc5cd8c485c52
Laiba Khan <80578072+LaibaKhan118@users.noreply.github.com>
2025-10-01 01:08:44 +0500

    Create Q1.cpp

---
+++ Lab5/Home_Tasks/Q1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int calculateFactorial(int n) {
+    if(n==1||n==0){
+        return 1;
+    }
+    return n*(calculateFactorial((n-1)));
+}
+int main()
+{
+    int n;
+    cout<< "Enter a number: ";
+    cin>> n;
+    int result= calculateFactorial(n);
+    cout<<"Fatorial: "<<result<<endl;
+    return 0;
+}
