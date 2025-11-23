980923e0bcf732c34e526ee1ee961ba84ccdf526
Laiba Khan <80578072+LaibaKhan118@users.noreply.github.com>
2025-11-23 13:15:20 +0500

    Update Q2.cpp

--- Lab11/Q2.cpp
+++ Lab11/Q2.cpp
@@ -2,18 +2,21 @@
 using namespace std;
 
 #define TSIZE 100
+
 class Node{
 public:
+    string key;
     string value;
     Node* next;
 
-    Node(const string& v) {
+    Node(string k, const string& v) {
+        key = k;
         value = v;
         next = nullptr;
     }
 };
 
-class HashTable {
+class Dictionary {
 private:
     Node* table[TSIZE];
     int hash(string key) {
@@ -25,34 +28,35 @@ private:
     }
 
 public:
-    HashTable() {
+    Dictionary() {
         for (int i = 0; i < TSIZE; i++)
         {
             table[i] = nullptr;
         }
         
     }
-    void Add_Record(const string& v) {
-        int index = hash(v);
+    void Add_Record(string k, const string& v) {
+        int index = hash(k);
         Node* curr = table[index];
 
         while(curr) {
-            if(curr->value == v) {
+            if(curr->key == k) {
                 curr->value = v;
                 return;
             }
             curr = curr->next;
         }
-        Node* newNode = new Node(v);
+        Node* newNode = new Node(k, v);
         newNode->next = table[index];
         table[index] = newNode;
     }
-    Node* Word_Search(string word) {
-        int index = hash(word);
+
+    Node* Word_Search(string key) {
+        int index = hash(key);
         Node* curr = table[index];
 
         while(curr) {
-            if(curr->value == word) {
+            if(curr->key == key) {
                 return curr;
             }
             curr = curr->next;
@@ -60,43 +64,60 @@ public:
         return nullptr;
     }
 
-    void display() {
-        for(int i = 0 ; i < TSIZE ; i++){
-             Node* temp = table[i];
-            if(temp == NULL){
-                    continue;
+    void Delete_Record(string key) {
+        int index = hash(key);
+        Node* curr = table[index];
+        Node *prev = nullptr;
+        while(curr) {
+            if(curr->key == key) {
+                if(!prev) {
+                    table[index] = curr->next;
                 }
-            cout << "[" << i << "]: " ;
-           
-            while(temp != NULL){
-                cout << "(" << temp->value << ")" ;
-                Node* curr = temp->next;
-
-                while(curr != NULL){
-                    cout << "->" ;
-                     cout << "(" << curr->value << ")" ;
-                     curr= curr->next;
+                else {
+                    prev->next = curr->next;
                 }
-                cout << endl;
-                temp = NULL;
+                delete curr;
+                cout << "Key " << key << " deleted Successfully!" << endl;
+                return;
             }
+            prev = curr;
+            curr = curr->next;
+        }
+        cout << "Error: Key Not Found!" << endl;
+    }
+
+    void Print_Dictionary() {
+        for(int i = 0 ; i < TSIZE ; i++){
+            if(table[i]) {
+                Node* temp = table[i];
+                cout << "Index " << i << ": " ;
             
+                while(temp){
+                    cout << "(" << temp->key << ", " << temp->value << ")";
+                    temp = temp->next;
+                }
+                cout << endl;            
+            }
         }
     }
 };
 
 int main(){
-    HashTable Htable;
-    Htable.Add_Record("aaaaaa");
-    Htable.Add_Record("bbbbb");
-    Htable.Add_Record("ccccc");
-    Htable.Add_Record("zzzzz");
-    cout << "Inserted!" << endl;
-    Node* find = Htable.Word_Search("aaaaaa");
+    Dictionary d;
+
+    d.Add_Record("AB", "FASTNU");
+    d.Add_Record("CD", "CS");
+    d.Add_Record("EF", "AI");
+    
+    Node *find = d.Word_Search("AB");
     if(find) {
-        cout << "Found:\nValue:" << find->value; 
+        cout << "Search key AB: " << find->value << endl;
     }
-   cout << endl;
-Htable.display();
+    else {
+        cout << "Search key AB: Not Found!" << endl;
+    }
+    d.Delete_Record("EF");
+    cout << "The complete Dictionary:" << endl;
+    d.Print_Dictionary();
     return 0;
 }
