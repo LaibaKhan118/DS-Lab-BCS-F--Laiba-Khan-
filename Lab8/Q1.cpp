0c176a01bb1993225fda7679622ece0f0b3dd069
Laiba Khan <80578072+LaibaKhan118@users.noreply.github.com>
2025-11-24 22:21:47 +0500

    Update Q1.cpp

--- Lab8/Q1.cpp
+++ Lab8/Q1.cpp
@@ -2,25 +2,23 @@
 #include <vector>
 using namespace std;
 
-class Node
-{
+class Node{
 public:
     int data;
     Node *left;
     Node *right;
 
-    Node(int v):data(v), left(nullptr), right(nullptr) {}
+    Node(int v) :data(v), left(nullptr), right(nullptr) {}
 };
 
-class BST
-{
+class BST{
 private:
     Node *root;
     Node *insert(Node *root, int v){
-        if (root == NULL)
+        if (root ==NULL)
             return new Node(v);
 
-        if (v < root->data)
+        if (v< root->data)
             root->left =insert(root->left, v);
         else
             root->right= insert(root->right, v);
@@ -28,114 +26,121 @@ private:
     }
 
     void inOrder(Node *root){
-        if (root == NULL)
+        if (root== NULL)
             return;
         inOrder(root->left);
-        cout<< root->data << " ";
+        cout << root->data << " ";
         inOrder(root->right);
     }
 
-    void inDOrder(Node *root){
+    void preOrder(Node *root){
+        if (root== NULL)
+            return;
+        cout<< root->data<< " ";
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+    void postOrder(Node *root){
         if (root == NULL)
             return;
-        inDOrder(root->right);
+        postOrder(root->left);
+        postOrder(root->right);
         cout << root->data << " ";
-        inDOrder(root->left);
     }
 
     Node *search(Node *root, int key){
         if (!root)
             return NULL;
-        if (root->data == key)
+        if (root->data ==key)
             return root;
-        else if (key < root->data)
+        else if (key< root->data)
             return search(root->left, key);
         else
             return search(root->right, key);
     }
 
     Node *InSuccessor(Node *root){
-        Node *curr = root;
-        while (curr && curr->left)
-            curr = curr->left;
-        return curr;
+    while (!root && !root->left)
+        root =root->left;
+    return root;
+}
+Node *delNode(Node *root, int key){
+    if (!root)
+        return NULL;
+    if (key< root->data){
+        root->left =delNode(root->left, key);
     }
-
-    Node *delNode(Node *root, int key){
-        if (!root)
-            return NULL;
-        if (key < root->data)
-            root->left = delNode(root->left, key);
-        else if (key > root->data)
-            root->right = delNode(root->right, key);
-        else
-        {
-            if (root->left == NULL)
-            {
-                Node *temp = root->right;
-                delete root;
-                return temp;
-            }
-            else if (root->right == NULL)
-            {
-                Node *temp = root->left;
-                delete root;
-                return temp;
-            }
-            else
-            {
-                Node *IS = InSuccessor(root->right);
-                root->data = IS->data;
-            }
+    else if (key >root->data){
+        root->right =delNode(root->right, key);
+    }
+    else{
+        if (root->left== NULL){
+            Node *temp= root->right;
+            delete root;
+            return temp;
         }
-        return root;
+        else if (root->right== NULL){
+            Node *temp =root->left;
+            delete root;
+            return temp;
+        }
+        else{
+            Node *IS= InSuccessor(root->right);
+            root->data =IS->data;
+            root->right =delNode(root->right, IS->data);
+        }
     }
+    return root;
+}
 
 public:
     BST() : root(nullptr) {}
-    void buildBST(vector<int> &arr)
-    {
+    void buildBST(vector<int> &arr){
         root = nullptr;
-        for (int i : arr)
-            root = insert(root, i);
+        for (int i :arr)
+            root= insert(root, i);
     }
-    void insert(int val)
-    {
-        root = insert(root, val);
+    void insert(int val){
+        root= insert(root, val);
     }
-    void deleteValue(int val)
-    {
-        root = delNode(root, val);
+    void deleteValue(int val){
+        root= delNode(root, val);
     }
-    bool search(int val)
-    {
+    bool search(int val){
         return search(root, val) != NULL;
     }
-    void printAscending(){
+    void printInOrder(){
         inOrder(root);
-        cout<< endl;
+        cout << endl;
     }
-    void printDescending(){
-        inDOrder(root);
-        cout<< endl;
+    void printPreOrder(){
+        preOrder(root);
+        cout << endl;
+    }
+    void printPostOrder(){
+        postOrder(root);
+        cout << endl;
     }
 };
 
-int main()
-{
-    vector<int> arr ={3, 2, 1, 5, 6, 4};
+int main(){
+    vector<int> arr = {3, 2, 1, 5, 6, 4};
 
     BST tree;
     tree.buildBST(arr);
 
-    cout << "Inorder (Ascending with heights): ";
-    tree.printAscending();
+    cout << "Preorder Traversal: ";
+    tree.printPreOrder();
+    cout << "Postorder Traversal: ";
+    tree.printPostOrder();
+    cout << "Inorder Traversal: ";
+    tree.printInOrder();
 
     cout << "\nDeleting 5...\n";
     tree.deleteValue(5);
 
     cout << "Inorder after deletion: ";
-    tree.printAscending();
+    tree.printInOrder();
     cout << "Searching for 4: " << (tree.search(4) ? "Found" : "Not Found") << endl;
 
     return 0;
